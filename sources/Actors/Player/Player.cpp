374dce03cadd3ae97b780474b30bc7ab0ce15f86
simon-gonand <simongonand@gmail.com>
2026-02-21 15:19:45 +0100

    tweak sliding falling acceleration and max speed

--- sources/Actors/Player/Player.cpp
+++ sources/Actors/Player/Player.cpp
@@ -79,7 +79,7 @@ void Player::Initialize()
 	MovementComp->AddNewMovementMode(EMovementMode::GROUND, std::make_shared<GroundMovementMode>(75.0f, 60.0f, 15.0f));
 	MovementComp->AddNewMovementMode(EMovementMode::JUMPING, std::make_shared<JumpingMovementMode>(56.0f, 10.0f, 15.0f, -15.0f, 2, MovementComp));
 	MovementComp->AddNewMovementMode(EMovementMode::FALLING, std::make_shared<FallingMovementMode>(56.0f, 10.0f, 15.0f, 30.0f, 1.5f));
-	MovementComp->AddNewMovementMode(EMovementMode::SLIDING, std::make_shared<SlidingMovementMode>(21.5f, 15.0f, 15.0f, MovementComp, 12.5f, 1.0f, 20.0f));
+	MovementComp->AddNewMovementMode(EMovementMode::SLIDING, std::make_shared<SlidingMovementMode>(21.5f, 15.0f, 15.0f, MovementComp, 12.5f, 20.0f, 25.0f));
 	MovementComp->AddNewMovementMode(EMovementMode::THROWN, std::make_shared<ThrownMovementMode>(PhysicsComp, MovementComp));
 	MovementComp->AddNewMovementMode(EMovementMode::GRAPPLING_THROWN, std::make_shared<GrapplingThrownMovementMode>(PhysicsComp, MovementComp, 75.0f, 20.0f, 20.0f, 0.5f));
 	MovementComp->AddNewMovementMode(EMovementMode::GRAPPLING_BALANCE, std::make_shared<GrapplingBalanceMovementMode>(15.0f, 3.0f, 15.0f, PhysicsComp, 0.25f));
